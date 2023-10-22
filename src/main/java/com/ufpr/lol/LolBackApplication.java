72b6235942a14319a4fe40f10ea3c1639a3bcd75
Thiago dos Santos <thiagodossantos315@gmail.com>
2023-10-22 20:15:22 -0300

    uptade

---
+++ src/main/java/com/ufpr/lol/LolBackApplication.java
@@ -0,0 +1,13 @@
+package com.ufpr.lol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolBackApplication.class, args);
+    }
+
+}
