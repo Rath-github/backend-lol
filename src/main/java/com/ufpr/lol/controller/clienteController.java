5b31471671497c69f001d8f180a05a44a094effd
Thiago dos Santos <thiagodossantos315@gmail.com>
2023-10-22 22:05:09 -0300

    Create Cliente Controller

---
+++ src/main/java/com/ufpr/lol/controller/clienteController.java
@@ -0,0 +1,19 @@
+package com.ufpr.lol.controller;
+
+import com.ufpr.lol.modal.ClienteModal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class clienteController {
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping("/criar")
+    public ResponseEntity<String> criarCliente(@RequestBody ClienteModal cliente) {
+        clienteService.criarCliente(cliente);
+        return ResponseEntity.ok("Cliente criado com sucesso!");
+    }
+}
