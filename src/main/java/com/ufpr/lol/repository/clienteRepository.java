c40a9333cf959c8f27bb3f16ea74de875bdfbcc3
Thiago dos Santos <thiagodossantos315@gmail.com>
2023-10-22 21:55:53 -0300

    Add Interface Cliente

---
+++ src/main/java/com/ufpr/lol/repository/clienteRepository.java
@@ -0,0 +1,7 @@
+package com.ufpr.lol.repository;
+
+import com.ufpr.lol.modal.ClienteModal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface clienteRepository extends JpaRepository<ClienteModal, Long> {
+}
