d73a4f3bcb5f1c3837f74ddfcc77343d0a8f04a5
Thiago dos Santos <thiagodossantos315@gmail.com>
2023-10-23 19:53:03 -0300

    Update Tb_Cliente

--- src/main/java/com/ufpr/lol/modal/ClienteModal.java
+++ src/main/java/com/ufpr/lol/modal/ClienteModal.java
@@ -1,16 +1,20 @@
 package com.ufpr.lol.modal;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
-@Entity
+import java.util.UUID;
 
+@Entity(name = "tb_clientes")
+@Data
 public class ClienteModal {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(generator = "uuid")
+    private UUID id;
     private String cpf;
     private String nome;
+    @Column(unique = true)
     private String email;
     private String endereco;
     private String telefone;
