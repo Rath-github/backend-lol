8bd06f68a09fb63b87b2386f48567bc50783b2ae
Thiago dos Santos <thiagodossantos315@gmail.com>
2023-10-22 21:57:53 -0300

    Create FindByMail

--- src/main/java/com/ufpr/lol/repository/clienteRepository.java
+++ src/main/java/com/ufpr/lol/repository/clienteRepository.java
@@ -4,4 +4,5 @@ import com.ufpr.lol.modal.ClienteModal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface clienteRepository extends JpaRepository<ClienteModal, Long> {
+    ClienteModal findByEmail(String email);
 }
